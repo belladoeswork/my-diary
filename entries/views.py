ca452b01feac879b401f16a1d27f69c4d4d3ba7a
belladoeswork <belladoeswork@gmail.com>
2022-04-29 04:43:40 +0800

    Display diary entries to the front end

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+
+from .models import Entry
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+class EntryDetailView(DetailView):
+    model = Entry
 
 # Create your views here.
