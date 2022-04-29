62fe4a300623f55c2dde3387326021f93e333d5f
belladoeswork <belladoeswork@gmail.com>
2022-04-29 16:26:00 +0800

    Create CRUD operations on the frontend

--- entries/views.py
+++ entries/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
 )
 
 from .models import Entry
